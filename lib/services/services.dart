acd3934c28473df491cf96069e37c6051950938d
Aziz Alfauzi <azizalfauzi@student.telkomuniversity.ac.id>
2022-11-29 09:20:50 +0700

    finishing layout and create new service

---
+++ lib/services/services.dart
@@ -0,0 +1,7 @@
+import 'package:recruitment_transisi_azizalfauzi/shared/shared_url.dart';
+
+import '../models/models.dart';
+import 'package:dio/dio.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+part 'user_services.dart';
+part 'auth_services.dart';
